138996191d68a9ae51e3aa7458c40447bbbc3b03
Hachem Zerdia <hachem.zerdia@gmail.com>
2014-06-17 14:38:23 +0100

    Formatting the code

--- core/classes/Documentation.php
+++ core/classes/Documentation.php
@@ -15,30 +15,30 @@ class Documentation {
     //    '1' => '../docs/initializing.html' 
     private $Docs = NULL;
 
-	public function __construct() {
-		$this->Docs = array(
-			'1' => BASE_PATH . '/docs/initializing.php',
-			'2' => BASE_PATH . '/docs/checkingStatus.php',
-			'3' => BASE_PATH . '/docs/addingAndCommitting.php',
-			'4' => BASE_PATH . '/docs/addingChanges.php',
-			'5' => BASE_PATH . '/docs/checkingForChanges.php',
-			'6' => BASE_PATH . '/docs/committing.php',
-			'7' => BASE_PATH . '/docs/addingAllChanges.php',
-			'8' => BASE_PATH . '/docs/committingAllChanges.php',
-			'9' => BASE_PATH . '/docs/history.php',
-		);
-	}
+    public function __construct() {
+        $this->Docs = array(
+            '1' => BASE_PATH . '/docs/initializing.php',
+            '2' => BASE_PATH . '/docs/checkingStatus.php',
+            '3' => BASE_PATH . '/docs/addingAndCommitting.php',
+            '4' => BASE_PATH . '/docs/addingChanges.php',
+            '5' => BASE_PATH . '/docs/checkingForChanges.php',
+            '6' => BASE_PATH . '/docs/committing.php',
+            '7' => BASE_PATH . '/docs/addingAllChanges.php',
+            '8' => BASE_PATH . '/docs/committingAllChanges.php',
+            '9' => BASE_PATH . '/docs/history.php',
+        );
+    }
 
-	/**
-	 * This function return challenge documentation.
-	 * 
-	 * @param $numOfChallenge {int} challenge number.
-	 * @return {string} documentation of challenge "html".
-	 * @author Hachem Zerdia
-	 **/
-	public function getDoc($numOfChallenge) {
+    /**
+     * This function return challenge documentation.
+     * 
+     * @param $numOfChallenge {int} challenge number.
+     * @return {string} documentation of challenge "html".
+     * @author Hachem Zerdia
+     **/
+    public function getDoc($numOfChallenge) {
         if (!empty($this->Docs[$numOfChallenge])) {
-		    //return file_get_contents($this->Docs[$numOfChallenge]);
+            //return file_get_contents($this->Docs[$numOfChallenge]);
             ob_start();
 
             require_once $this->Docs[$numOfChallenge];
@@ -49,7 +49,7 @@ class Documentation {
         } else {
             return null;
         }
-	}
+    }
 
     function __destruct() {
         $this->Docs = NULL;
