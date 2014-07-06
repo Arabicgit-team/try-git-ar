cc484df4bc8d5a886e90332d0166c5b7d43f3ca8
Hachem Zerdia <hachem.zerdia@gmail.com>
2014-07-06 13:13:46 +0100

    Fix bug name of file is wrong

---
+++ docs/committingBranchChanges.php
@@ -0,0 +1,21 @@
+<div id="doc-template">
+    <h1>
+        <em>21.</em> 
+        <?php echo _("Commiting Branch Changes"); ?>
+    </h1>
+    <div id="challenge-descroption">
+        <p>
+            <?php echo _("Now that you've removed all the cats you'll need to 
+            commit your changes."); ?>
+        </p>
+        <p>
+            <?php  echo _("Feel free to run git status to check the changes 
+            you're about to commit."); ?>
+        </p>
+        <p>
+            <code dir="ltr" class="input-command">
+                git commit -m "Remove all the cats"
+            </code>
+        </p>
+    </div>
+</div>
